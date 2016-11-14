8ce2680e4ac1f58d21af5a72de18ac2f10d47fc0
huangchen <1440557925@qq.com>
2016-11-14 17:06:14 +0800

    更新springboot版本

--- src/main/java/com/windhc/config/ShiroRealmImpl.java
+++ src/main/java/com/windhc/config/ShiroRealmImpl.java
@@ -14,32 +14,32 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class ShiroRealmImpl extends AuthorizingRealm {
 
-  @Autowired
-  private UserService userService;
+    @Autowired
+    private UserService userService;
 
-  // 用于授权
-  @Override
-  protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-    System.out.println("doGetAuthorizationInfo(PrincipalCollection principals)");
-    principals.getRealmNames().forEach(str -> System.out.println(str));
-    return null;
-  }
-
-  // 用于认证
-  @Override
-  protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-    String username = (String)token.getPrincipal();
-    User user = userService.findByUsername(username);
-
-    if(user == null) {
-      throw new UnknownAccountException();  //没找到帐号
+    // 用于授权
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        System.out.println("doGetAuthorizationInfo(PrincipalCollection principals)");
+        principals.getRealmNames().forEach(str -> System.out.println(str));
+        return null;
     }
-    //交给AuthenticatingRealm使用CredentialsMatcher进行密码匹配，如果觉得人家的不好可以自定义实现
-    return new SimpleAuthenticationInfo(
-        user,
-        user.getPassword(), //密码
+
+    // 用于认证
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        String username = (String) token.getPrincipal();
+        User user = userService.findByUsername(username);
+
+        if (user == null) {
+            throw new UnknownAccountException();  //没找到帐号
+        }
+        //交给AuthenticatingRealm使用CredentialsMatcher进行密码匹配，如果觉得人家的不好可以自定义实现
+        return new SimpleAuthenticationInfo(
+                user,
+                user.getPassword(), //密码
 //        ByteSource.Util.bytes(user.getSalt()),//salt  加密使用的盐
-        getName()  //realm name
-    );
-  }
+                getName()  //realm name
+        );
+    }
 }
--- src/test/java/com/windhc/BlogApplicationTests.java
+++ src/test/java/com/windhc/BlogApplicationTests.java
@@ -2,12 +2,13 @@ package com.windhc;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = BlogApplication.class)
+//@SpringApplicationConfiguration(classes = BlogApplication.class)
+@SpringBootTest
 @WebAppConfiguration
 public class BlogApplicationTests {
 
